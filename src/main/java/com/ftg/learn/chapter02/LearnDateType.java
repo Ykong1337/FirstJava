abbfc6dcdefad9d0db10729901f8f81ae791ee28
Ykong <ykonghao@icloud.com>
2021-07-06 16:19:37 +0800

    second

---
+++ src/main/java/com/ftg/learn/chapter02/LearnDateType.java
@@ -0,0 +1,35 @@
+package com.ftg.learn.chapter02;
+
+/**
+ * 数据类型
+ *
+ * @author YKONG
+ */
+public class LearnDateType {
+    public static void main(String[] args) {
+        byte byte1 = -128;
+        byte byte2 = 127;
+        byte byte3 = 0;
+        short short1 = 3;
+        short short2 = 9999;
+        short short3 = 32767;
+        int int1 = 'A';
+        int int2 = 2;
+        int int3 = 1;
+        long long1 = 0;
+        long long2 = 3;
+        int int5 = (int) long2;
+        float float1 = 'A';
+        float float3 = 2.5f;
+        float float2 = -33;
+        double double1 = 2.3;
+        double double2 = 'A';
+        char char1 = '9';
+        char char2 = 'A';
+        char char3 = 3;
+        char char4 = 65;
+        boolean boolean1 = false;
+        boolean boolean2 = true;
+        System.out.println(float1);
+    }
+}

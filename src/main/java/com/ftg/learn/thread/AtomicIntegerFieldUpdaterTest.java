6583addb24f8c7cbc8fed2f8a509eea08b9fb895
ykong <ykonghao@icloud.com>
2021-10-20 10:57:15 +0800

    last

---
+++ src/main/java/com/ftg/learn/thread/AtomicIntegerFieldUpdaterTest.java
@@ -0,0 +1,78 @@
+package com.ftg.learn.thread;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.testng.annotations.Test;
+
+import java.io.Serializable;
+import java.util.concurrent.atomic.AtomicIntegerFieldUpdater;
+import java.util.concurrent.atomic.AtomicLongFieldUpdater;
+import java.util.concurrent.atomic.AtomicReferenceFieldUpdater;
+
+public class AtomicIntegerFieldUpdaterTest {
+
+    /**
+     * 测试引用类型原子类
+     */
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    private static class Student implements Serializable {
+        volatile long id;
+        volatile String name;
+        volatile int age;
+    }
+
+    @Test
+    public void testAtomicIntegerFieldUpdater() {
+        Student student = new Student(1L, "赵四", 58);
+
+        // 使用ageUpdater绑定Student类中的age属性
+        AtomicIntegerFieldUpdater<Student> ageUpdater =
+                AtomicIntegerFieldUpdater.newUpdater(Student.class, "age");
+
+        // 对student中的age++并返回
+        System.out.println("age++: " + ageUpdater.getAndIncrement(student));
+        System.out.println("age--: " + ageUpdater.getAndDecrement(student));
+        System.out.println("先返回age的值，然后 age+=5: " + ageUpdater.getAndAdd(student, 5));
+        System.out.println("先返回age的值，然后 accumulate: " + ageUpdater.getAndAccumulate(student, 5, (a, b) -> {
+            System.out.printf("a: %s b: %s\n", a, b);
+            return a + b;
+        }));
+    }
+
+    @Test
+    public void testAtomicLongFieldUpdater() {
+        Student student = new Student(1L, "赵四", 58);
+
+        // 使用idUpdater绑定Student类中的id属性
+        AtomicLongFieldUpdater<Student> idUpdater =
+                AtomicLongFieldUpdater.newUpdater(Student.class, "id");
+
+        System.out.println("id: " + student.getId());
+        System.out.println("++id: " + idUpdater.incrementAndGet(student));
+        System.out.println("--id: " + idUpdater.decrementAndGet(student));
+        System.out.println("id+=5: " + idUpdater.addAndGet(student, 5));
+        System.out.println("accumulate: " + idUpdater.accumulateAndGet(student, 5, (a, b) -> {
+            System.out.printf("a: %s b: %s\n", a, b);
+            return a + b;
+        }));
+    }
+
+    @Test
+    public void testAtomicReferenceFieldUpdater() {
+        Student student = new Student(1L, "赵四", 58);
+
+        // 使用nameUpdater绑定Student类中的name属性
+        AtomicReferenceFieldUpdater<Student, String> nameUpdater =
+                AtomicReferenceFieldUpdater.newUpdater(Student.class, String.class, "name");
+
+        System.out.println("将name修改为飞机: " + nameUpdater.getAndSet(student, "飞机"));
+        System.out.println("student: " + student);
+
+        System.out.println("CAS方式将name从飞机修改为大炮: "
+                + nameUpdater.compareAndSet(student, "飞机", "大炮"));
+        System.out.println("student: " + student);
+    }
+}
---
+++ src/main/java/com/ftg/learn/thread/ReentrantLockTest.java
@@ -0,0 +1,136 @@
+package com.ftg.learn.thread;
+
+import lombok.SneakyThrows;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class ReentrantLockTest {
+
+    /**
+     * 可重入锁测试：添加重入锁/释放锁
+     */
+    private static class LockDemo implements Runnable {
+
+        /**
+         * 创建一个可重入锁实例
+         */
+        private Lock lock = new ReentrantLock();
+
+        /**
+         * 线程体
+         */
+        @Override
+        public void run() {
+            lock.lock();
+            try {
+
+                // 打印5遍当前线程
+                for (int i = 0; i < 5; i++) {
+                    TimeUnit.SECONDS.sleep(1L);
+                    System.out.println(Thread.currentThread().getName() + ": " + i);
+                }
+
+                // 调用method()方法，测试ReentrantLock是否可重入
+                method();
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                lock.unlock();
+            }
+        }
+
+        /**
+         * 方法内使用lock进行上锁和解锁
+         */
+        private void method() {
+            lock.lock();
+            try {
+                System.out.println(Thread.currentThread().getName() + ": 执行完毕");
+            } finally {
+                // 释放锁的代码必须写在finally块中
+                lock.unlock();
+            }
+        }
+    }
+
+    /**
+     * 可重入锁测试：尝试获取锁
+     */
+    private static class TryLockDemo {
+
+        /**
+         * 创建一个可重入锁实例
+         */
+        private Lock lock = new ReentrantLock();
+
+        /**
+         * 每隔1s打印1次当前线程，共打印五次
+         */
+        private void methodA() {
+            lock.lock();
+            try {
+                for (int i = 0; i < 5; i++) {
+                    TimeUnit.SECONDS.sleep(1L);
+                    System.out.println(Thread.currentThread().getName() + ": " + i);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                lock.unlock();
+            }
+        }
+
+        /**
+         * 尝试在3秒内获取锁
+         */
+        private void methodB() {
+            // 记录是否获取到了锁
+            boolean locked = false;
+            try {
+                // 3秒内不断尝试获取锁，该过程不阻塞线程
+                locked = lock.tryLock(3, TimeUnit.SECONDS);
+                System.out.println(locked ?
+                        Thread.currentThread().getName() + ": 获取到了锁..." :
+                        Thread.currentThread().getName() + ": 没有获取到锁..." );
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                if (locked) {
+                    lock.unlock();
+                }
+            }
+        }
+    }
+
+    @Test
+    @SneakyThrows
+    public void testLock() {
+        LockDemo lockDemo = new LockDemo();
+        new Thread(lockDemo).start();
+        new Thread(lockDemo).start();
+
+        // 阻塞junit线程
+        TimeUnit.SECONDS.sleep(20);
+    }
+
+    @Test
+    @SneakyThrows
+    public void testTryLock() {
+        TryLockDemo trylockDemo = new TryLockDemo();
+
+        // 启动一个线程，执行trylockDemo实例中的methodA()
+        new Thread(trylockDemo::methodA).start();
+
+        TimeUnit.SECONDS.sleep(1L);
+
+        // 启动一个线程，执行trylockDemo实例中的methodB()
+        new Thread(trylockDemo::methodB).start();
+
+        // 阻塞junit线程
+        TimeUnit.SECONDS.sleep(20);
+    }
+
+}
